6a1fb46365de7be09909460c6efcd8d482810f33
Daseanle <daseanleelee@gmail.com>
2025-12-19 20:36:51 +0800

    Update page.js

--- app/redirect/page.js
+++ app/redirect/page.js
@@ -1,7 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function RedirectPage() {
-  redirect(
-    "https://promptchan.com/m/qVPRhRSDL6NaEN5HHWaLq8pk5Jo2"
-  );
+  redirect("https://promptchan.com/m/qVPRhRSDL6NaEN5HHWaLq8pk5Jo2");
 }
